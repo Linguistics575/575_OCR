8ca2304db11029473132f08e88cf8b61f95b4531
Eric Lindberg <lindbe2@uw.edu>
2018-04-18 16:43:31 -0700

    Fixing some of the regular expression matching.

--- util/scripts/convert_and_scan.sh
+++ util/scripts/convert_and_scan.sh
@@ -2,3 +2,4 @@
 
 ./get_page_images.sh $1 $2
 ./scan_pages.sh $2
+rm $2/*.tiff
--- util/scripts/get_page_images.sh
+++ util/scripts/get_page_images.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-convert -density 300 $1 -strip -background white -alpha off $2/1848-martineau-eastern-life_%04d.tiff
+fn=$(basename $1)
+
+convert -density 300 $1 -strip -background white -alpha off $2/${fn%.*}_%04d.tiff
