68236e1e24fa469ce511cc78ee6630abd3aad382
Eric Lindberg <lindbe2@uw.edu>
2018-05-31 09:13:17 -0700

    And remember to include the new stuff!

---
+++ util/scripts/SCAN_JPG_DIR.cmd
@@ -0,0 +1,28 @@
+#------------------------------------------------------------------------
+# Run the scan_from_pdf script on
+# a the list of *.pdf files provided
+# 
+# script command description (see scan_from_pdf.py -h)
+# usage: scan_from_pdf.py [-h] [-r R] [-i I] [-o O] [-p P] [-l L] [-f F [F ...]] [-g]
+#
+# Scan PDF images and place pages into directory
+#
+# optional arguments:
+#   -h, --help  show this help message and exit
+#   -r R        Image resolution (at least 100, 600+ recommended)
+#   -i I        Input Directory containing PDF files
+#   -o O        Output Directory to which collections of text files are written
+#   -p P        Start page number (default is 0)
+#   -l L        Language being read (default is "eng")
+#   -f F [F ...] PDF image files to be scanned (default is all)
+#   -d          Turn on debug messages
+#------------------------------------------------------------------------
+universe	= vanilla
+executable 	= scan_pages.sh
+getenv		= true
+output		= scan.out
+error		= scan.err
+log		= scan.log
+arguments	= "~/Workspace/ling575/1846-romer-pilgrimage-to-temples-tombs-v1 ~/Workspace/ling575/575_OCR/raw_texts/1846-romer-pilgrimage-to-temples-tombs-v1"
+transfer_executable = false
+queue
